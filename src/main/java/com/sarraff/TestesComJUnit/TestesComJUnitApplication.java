d56fea31e86cb3fac77012c12a6798309f86c624
Paulo Sarraff Pereira Júnior <ppereira.deal@veloe.com.br>
2023-01-26 18:09:15 -0300

    Commit inicial

---
+++ src/main/java/com/sarraff/TestesComJUnit/TestesComJUnitApplication.java
@@ -0,0 +1,13 @@
+package com.sarraff.TestesComJUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesComJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesComJUnitApplication.class, args);
+	}
+
+}
