57c7e8eaaed670eba8509834ee4cd1a325501b89
Paulo Sarraff Pereira Júnior <ppereira.deal@veloe.com.br>
2023-01-26 20:48:53 -0300

    Pequena alteração na classe user

--- src/main/java/com/sarraff/TestesComJUnit/domain/User.java
+++ src/main/java/com/sarraff/TestesComJUnit/domain/User.java
@@ -1,11 +1,27 @@
 package com.sarraff.TestesComJUnit.domain;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	
+	@Column(unique = true)
 	private String email;
 	private String password;
 }
