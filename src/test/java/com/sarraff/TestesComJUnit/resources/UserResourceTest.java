2263146c3fbf8dfc27fc250d47fcf6f87ec37e8e
Paulo Sarraff Pereira Júnior <ppereira.deal@veloe.com.br>
2023-02-08 14:59:19 -0300

    classe de teste para usuarioResource iniciada

---
+++ src/test/java/com/sarraff/TestesComJUnit/resources/UserResourceTest.java
@@ -0,0 +1,24 @@
+package com.sarraff.TestesComJUnit.resources;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserResourceTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
