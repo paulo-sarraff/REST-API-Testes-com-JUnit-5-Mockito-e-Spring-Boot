3606ce0bc2b53687e3692e4995cac0701563cbc3
Paulo Sarraff Pereira Júnior <ppereira.deal@veloe.com.br>
2023-02-08 10:32:20 -0300

    Teste para classe usuarioService metodo findAll

--- src/test/java/com/sarraff/TestesComJUnit/services/impl/UsuarioServiceImplTest.java
+++ src/test/java/com/sarraff/TestesComJUnit/services/impl/UsuarioServiceImplTest.java
@@ -2,10 +2,10 @@ package com.sarraff.TestesComJUnit.services.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -25,6 +25,8 @@ import com.sarraff.TestesComJUnit.services.exceptions.ObjectNotFoundException;
 @SpringBootTest
 class UsuarioServiceImplTest {
 	
+	private static final int INDEX = 0;
+
 	private static final String PASSWORD = "123";
 
 	private static final String EMAIL = "paulo@uea.edu.br";
@@ -83,7 +85,18 @@ class UsuarioServiceImplTest {
 	}
 
 	@Test
-	void testFindAll() {
+	void whenFindAllThenReturnAnListOfUsers() {
+		when(usuarioRepository.findAll()).thenReturn(List.of(usuario));
+		List<Usuario> response = service.findAll();
+		
+		assertNotNull(response);
+		assertEquals(1, response.size());
+		assertEquals(Usuario.class, response.get(INDEX).getClass());
+		
+		assertEquals(ID, response.get(INDEX).getId());
+		assertEquals(NOME, response.get(INDEX).getName());
+		assertEquals(EMAIL, response.get(INDEX).getEmail());
+		assertEquals(PASSWORD, response.get(INDEX).getPassword());
 	}
 
 	@Test
